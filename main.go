1dede19d2d6b6b0a99de279cdd4eb12dec57a3c2
Daniel Carneiro <geral4x@gmail.com>
2023-05-02 23:16:38 +0100

    chore: refactored code to use chi instead of gin

--- main.go
+++ main.go
@@ -1,186 +1,297 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"fmt"
+	"encoding/json"
+	bookTypes "go-server/models"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	_ "github.com/lib/pq"
 )
 
-// DB holds the database connection
-var DB *sql.DB
-
-
-// Book represents a book object
-type book struct {
-	ID     		string 	`json:"id"`
-	Title  		string 	`json:"title"`
-	Author 		string 	`json:"author"`
-	Quantity 	int 	`json:"quantity"`
+// Database is a struct for managing database connections
+type Database struct {
+    *sql.DB
 }
+
+// Error is a custom error type for database errors
+type Error string
+
+func (e Error) Error() string {
+    return string(e)
+}
+
+// Common database errors
+const (
+    ErrNotFound = Error("not found")
+    ErrInternal = Error("internal error")
+)
 
 // Returns a list of all books
-func getBooks(c *gin.Context) {
-	rows, err := DB.Query("SELECT id, title, author, quantity FROM books")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer rows.Close()
+func (db *Database) GetBooks(ctx context.Context) ([]bookTypes.Book, error) {
+    rows, err := db.QueryContext(ctx, "SELECT id, title, author, quantity FROM books")
+    if err != nil {
+        return nil, ErrInternal
+    }
+    defer rows.Close()
 
-	var books []book
-	for rows.Next() {
-		var b book
-		err := rows.Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		books = append(books, b)
-	}
+    var books []bookTypes.Book
+    for rows.Next() {
+        var b bookTypes.Book
+        err := rows.Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
+        if err != nil {
+            return nil, ErrInternal
+        }
+        books = append(books, b)
+    }
 
-	c.JSON(http.StatusOK, books)
+    if err = rows.Err(); err != nil {
+        return nil, ErrInternal
+    }
+
+    return books, nil
 }
 
-func getBook(c *gin.Context) {
-    id := c.Param("id")
-    
-    var b book
-    
-    err := DB.QueryRow("SELECT id, title, author, quantity FROM books WHERE id=$1", id).Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
+// Returns a single book with the given ID
+func (db *Database) GetBook(ctx context.Context, id string) (*bookTypes.Book, error) {
+    var b bookTypes.Book
+
+    err := db.QueryRowContext(ctx, "SELECT id, title, author, quantity FROM books WHERE id=$1", id).Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
     if err != nil {
         if err == sql.ErrNoRows {
-            c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
-            return
+            return nil, ErrNotFound
         }
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
+        return nil, ErrInternal
     }
-    
-    c.JSON(http.StatusOK, b)
+
+    return &b, nil
 }
 
 // Creates a new book and adds it to the database
-func addBook(c *gin.Context) {
-	var newBook book
+func (db *Database) AddBook(ctx context.Context, newBook *bookTypes.Book) (*bookTypes.Book, error) {
+    _, err := db.ExecContext(ctx, "INSERT INTO books (id, title, author, quantity) VALUES ($1, $2, $3, $4)", newBook.ID, newBook.Title, newBook.Author, newBook.Quantity)
+    if err != nil {
+        return nil, ErrInternal
+    }
 
-	if err := c.ShouldBindJSON(&newBook); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	sqlStatement := `INSERT INTO books (id, title, author, quantity) VALUES ($1, $2, $3, $4)`
-	_, err := DB.Exec(sqlStatement, newBook.ID, newBook.Title, newBook.Author, newBook.Quantity)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusCreated, newBook)
-}
-
-func viewAllTables(c *gin.Context) {
-	rows, err := DB.Query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	
-	var tables []string
-	for rows.Next() {
-		var tableName string
-		err := rows.Scan(&tableName)
-		if err != nil {
-			panic(err)
-		}
-		tables = append(tables, tableName)
-	}
-	
-	if err = rows.Err(); err != nil {
-		panic(err)
-	}
-	
-	fmt.Println(tables)
+    return newBook, nil
 }
 
 // Deletes a book from the database
-func deleteBook(c *gin.Context) {
-	id := c.Param("id")
+func (db *Database) DeleteBook(ctx context.Context, id string) error {
+    res, err := db.ExecContext(ctx, "DELETE FROM books WHERE id=$1", id)
+    if err != nil {
+        return ErrInternal
+    }
 
-	// Delete book from the database
-	res, err := DB.Exec("DELETE FROM books WHERE id=$1", id)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+    rowsAffected, err := res.RowsAffected()
+    if err != nil {
+        return ErrInternal
+    }
 
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+    if rowsAffected == 0 {
+        return ErrNotFound
+    }
 
-	if rowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Book deleted"})
+    return nil
 }
 
-func updateBook(c *gin.Context) {
-	id := c.Param("id")
+// Updates a book in the database
+func (db *Database) UpdateBook(ctx context.Context, id string, updatedBook *bookTypes.Book) (*bookTypes.Book, error) {
+    var b bookTypes.Book
+    row := db.QueryRowContext(ctx, "SELECT id, title, author, quantity FROM books WHERE id = $1", id)
+    err := row.Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            return nil, ErrNotFound
+        }
+        return nil, ErrInternal
+    }
 
-	var updatedBook book
-	if err := c.ShouldBindJSON(&updatedBook); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+    b.Title = updatedBook.Title
+    b.Author = updatedBook.Author
+    b.Quantity = updatedBook.Quantity
 
-	var b book
-	row := DB.QueryRow("SELECT id, title, author, quantity FROM books WHERE id = $1", id)
-	err := row.Scan(&b.ID, &b.Title, &b.Author, &b.Quantity)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+    _, err = db.ExecContext(ctx, "UPDATE books SET title=$1, author=$2, quantity=$3 WHERE id=$4", b.Title, b.Author, b.Quantity, b.ID)
+    if err != nil {
+        return nil, ErrInternal
+    }
 
-	b.Title = updatedBook.Title
-	b.Author = updatedBook.Author
-	b.Quantity = updatedBook.Quantity
+    return &b, nil
+}
 
-	_, err = DB.Exec("UPDATE books SET title=$1, author=$2, quantity=$3 WHERE id=$4", b.Title, b.Author, b.Quantity, b.ID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+// Views all tables in the public schema
+func (db *Database) ViewAllTables(ctx context.Context) ([]string, error) {
+    rows, err := db.QueryContext(ctx, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    if err != nil {
+        return nil, ErrInternal
+    }
+    defer rows.Close()
 
-	c.JSON(http.StatusOK, b)
+    var tables []string
+    for rows.Next() {
+        var tableName string
+        err := rows.Scan(&tableName)
+        if err != nil {
+            return nil, ErrInternal
+        }
+        tables = append(tables, tableName)
+    }
+
+    if err = rows.Err(); err != nil {
+        return nil, ErrInternal
+    }
+
+    return tables, nil
 }
 
 func main() {
+
+    // Initialize database
+    dsn := "postgres://gouser:admingres@localhost:5432/godb?sslmode=disable"
+    db, err := sql.Open("postgres", dsn)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    database := &Database{db}
+
+    // Initialize router
+    router := chi.NewRouter()
+    router.Use(middleware.Logger)
+    router.Use(middleware.Recoverer)
+
+    // API endpoints
+    router.Get("/tables", func(w http.ResponseWriter, r *http.Request) {
+        tables, err := database.ViewAllTables(r.Context())
+        if err != nil {
+            switch err {
+            case ErrNotFound:
+                w.WriteHeader(http.StatusNotFound)
+            case ErrInternal:
+                w.WriteHeader(http.StatusInternalServerError)
+            }
+            return
+        }
+
+        json.NewEncoder(w).Encode(tables)
+    })
+
+    router.Get("/books", func(w http.ResponseWriter, r *http.Request) {
+        books, err := database.GetBooks(r.Context())
+        if err != nil {
+            switch err {
+            case ErrInternal:
+                w.WriteHeader(http.StatusInternalServerError)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+            }
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        json.NewEncoder(w).Encode(books)
+    })
+
+    router.Get("/books/{id}", func(w http.ResponseWriter, r *http.Request) {
+        id := chi.URLParam(r, "id")
+
+        b, err := database.GetBook(r.Context(), id)
+        if err != nil {
+            switch err {
+			case ErrNotFound:
+				w.WriteHeader(http.StatusNotFound)
+				http.Error(w, "book not found", http.StatusNotFound)
+            case ErrInternal:
+                w.WriteHeader(http.StatusInternalServerError)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+            }
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        json.NewEncoder(w).Encode(b)
+    })
+
+    router.Post("/books", func(w http.ResponseWriter, r *http.Request) {
+        var newBook bookTypes.Book
+        err := json.NewDecoder(r.Body).Decode(&newBook)
+        if err != nil {
+            w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "error - bad request", http.StatusBadRequest)
+            return
+        }
+
+        b, err := database.AddBook(r.Context(), &newBook)
+        if err != nil {
+            switch err {
+            case ErrInternal:
+                w.WriteHeader(http.StatusInternalServerError)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+
+            }
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusCreated)
+        json.NewEncoder(w).Encode(b)
+    })
+
+    router.Put("/books/{id}", func(w http.ResponseWriter, r *http.Request) {
+        id := chi.URLParam(r, "id")
+
+        var updatedBook bookTypes.Book
+        if err := json.NewDecoder(r.Body).Decode(&updatedBook); err != nil {
+            w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "error - bad request", http.StatusBadRequest)
+            return
+        }
+
+        b, err := database.UpdateBook(r.Context(), id, &updatedBook)
+        if err != nil {
+            switch err {
+            case ErrNotFound:
+				w.WriteHeader(http.StatusNotFound)
+				http.Error(w, "book not found", http.StatusNotFound)
+            case ErrInternal:
+                w.WriteHeader(http.StatusInternalServerError)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+            }
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusOK)
+        json.NewEncoder(w).Encode(b)
+    })
+
+	router.Delete("/books/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
 	
-	router := gin.Default()
-	dsn := "postgres://gouser:admingres@localhost:5432/godb?sslmode=disable"
+		ctx := r.Context()
+		err := database.DeleteBook(ctx, id)
+		if err != nil {
+			switch err {
+			case ErrNotFound:
+				w.WriteHeader(http.StatusNotFound)
+				http.Error(w, "book not found", http.StatusNotFound)
+			case ErrInternal:
+				w.WriteHeader(http.StatusInternalServerError)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+			}
+			return
+		}
+	
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"message": "book deleted"})
+	})
 
-	var err error
-	DB, err = sql.Open("postgres", dsn)
-	if err != nil {
-		panic(err)
-	}
-
-	router.GET("/tables", viewAllTables)
-	router.GET("/books", getBooks)
-	router.GET("/books/:id", getBook)
-	router.POST("/books", addBook)
-	router.PUT("/books/:id", updateBook)
-	router.DELETE("/books/:id", deleteBook)
-
-	router.Run( "localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
